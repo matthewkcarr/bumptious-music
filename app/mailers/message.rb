36e55fa1a5ded8386910bd525a4dea5be9df1880
Matt Carr <bikokid@gmail.com>
2011-02-19 18:17:51 -0800

    messaging

--- app/mailers/message.rb
+++ app/mailers/message.rb
@@ -3,6 +3,7 @@ class Message < ActionMailer::Base
   def message_us(data)
     @content = data
     mail(:from => 'matt@bumptiousmusic.com', 
+         :to => 'matt@bumptiousmusic.com',
          :reply => data[:email],
          :subject => "A fan sent you a message")
   end
