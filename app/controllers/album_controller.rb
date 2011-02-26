a54f0056917a4954890765da982a3d7e0ef0e83f
Matt Carr <bikokid@gmail.com>
2011-02-26 15:19:01 -0800

    using new zips

--- app/controllers/album_controller.rb
+++ app/controllers/album_controller.rb
@@ -13,10 +13,10 @@ class AlbumController < ApplicationController
       format.zip {
         if params[:id].include?('m4a')
           dname = 'Bumptious - Remix Elixirs Album LOSSLESS M4U' + '.zip'
-          send_file RAILS_ROOT + '/public/music/album/remix_elixir_m4a.zip', :filename => dname, :type=>"application/force-download"
+          send_file RAILS_ROOT + '/public/music/album/new_m4a.zip', :filename => dname, :type=>"application/force-download"
         else
           dname = 'Bumptious - Remix Elixirs Album LOSSY MP3' + '.zip'
-          send_file RAILS_ROOT + '/public/music/album/remix_elixir_mp3.zip', :filename => dname, :type=>"application/force-download"
+          send_file RAILS_ROOT + '/public/music/album/new_mp3.zip', :filename => dname, :type=>"application/force-download"
         end
       }
     end
